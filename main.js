ea4310d3836b2f958f4911bc8f64d2d6cfa9eed9
Ariel Liu <cutiepanda2468@gmail.com>
2024-06-24 10:54:32 -0400

    scene, camera, geometry creation and rendering

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+import * as THREE from "three";
 
+// Scene
+const scene = new THREE.Scene();
+
+// create sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83",
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(45, 800, 600);
+scene.add(camera);
+
+// Renderer - need to render scene on screen using canvas
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGL1Renderer({ canvas });
+renderer.setSize(800, 600);
+renderer.render(scene, camera);
