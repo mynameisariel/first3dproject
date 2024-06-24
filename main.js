e0defb396d7d08a0c422b76cbb5f123936b83838
Ariel Liu <cutiepanda2468@gmail.com>
2024-06-24 16:51:51 -0400

    rendering and resizing when window is resized

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import "./style.css";
 
 // Scene
 const scene = new THREE.Scene();
@@ -11,18 +12,48 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
 // Light
-const light = new THREE.PointLight(0xffffff, 70, 100);
+const light = new THREE.PointLight(0xffffff, 70, 100, 1.7);
 light.position.set(0, 10, 10);
 scene.add(light);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 200);
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
 camera.position.z = 10;
 scene.add(camera);
 
 // Renderer - need to render scene on screen using canvas
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+// Resize
+window.addEventListener("resize", () => {
+  // update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // update camera and renderer
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+  //console.log(sizes.width);
+});
+
+const loop = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+loop();
