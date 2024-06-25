552f041f51ff4e60422a47718560277737cf0602
Ariel Liu <cutiepanda2468@gmail.com>
2024-06-25 10:40:43 -0400

    colour changing sphere animations, final touches

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ const scene = new THREE.Scene();
 // create sphere
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
-  color: "#00ff83",
+  color: "#ffffff",
+  roughness: 0.5,
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
@@ -21,7 +22,7 @@ const sizes = {
 };
 
 // Light
-const light = new THREE.PointLight(0xffffff, 70, 100, 1.7);
+const light = new THREE.PointLight(0xffffff, 50, 100, 1.7);
 light.position.set(0, 10, 10);
 scene.add(light);
 
@@ -73,3 +74,28 @@ loop();
 // Timeline
 const tl = gsap.timeline({ defaults: { duration: 1 } });
 tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
+tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
+
+// Mouse animation colour
+let mouseDown = false;
+let rgb = [];
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ];
+    // animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
